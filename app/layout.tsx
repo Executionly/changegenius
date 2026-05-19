b5baf05b2f067de7b261d3379ba2e3573fcea864
emmastyler <emmastyler1998@gmail.com>
2026-05-19 18:43:05 +0100

    favicon-inserted

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/logo.png" sizes="any" />
         <script
           src="//code.tidio.co/04hpwbat73cpohjtwkvw7uaao13b31wv.js"
           async
